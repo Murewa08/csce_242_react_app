7140cf91d66697e25dcaf43e671cc7a18990cedb
Murewa <adebajomurewa@gmail.com>
2026-03-26 10:12:41 -0400

    Added Map from Components to Help

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,8 +3,8 @@ import "../css/Map.css";
 const Map = () => {
     return(
         <div id="map">
-            <p>123 Anywhere Street</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1654.0031017187341!2d-81.03231193103517!3d33.99237441950589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f8a4d455555555%3A0x50c181a3ce26e9a9!2sIBM%20Center%20For%20Innovation%20UofSC!5e0!3m2!1sen!2sus!4v1774358762954!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p>573 Oakland Street</p>
+            <iframe height="370" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1654.0031017187341!2d-81.03231193103517!3d33.99237441950589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f8a4d455555555%3A0x50c181a3ce26e9a9!2sIBM%20Center%20For%20Innovation%20UofSC!5e0!3m2!1sen!2sus!4v1774358762954!5m2!1sen!2sus" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     );
 };
--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,4 +1,5 @@
 import "../css/Help.css";
+import Map from "../components/Map";
 
 const Help = () => {
     return (
@@ -33,9 +34,7 @@ const Help = () => {
                 </form>
             </div>
             <div className="box" id="google-map">
-                <h1>Headquarters Location</h1>
-                <p>573 Oakland Street</p>
-                <iframe id="map" title="Map describing headquaters location" src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3308.0021038412383!2d-81.03373772523273!3d33.992479720885186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f8bad7ff85decd%3A0xeba1e7740d5c10e0!2sM.%20Bert%20Storey%20Engineering%20and%20Innovation%20Center!5e0!3m2!1sen!2sus!4v1772547121327!5m2!1sen!2sus"} style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <Map/>
             </div>
         </main>
         {/*<script src="script.js"></script>*/}
