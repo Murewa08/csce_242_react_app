582c4ada5f05bf28560cd4cb81a816e0580bba4b
Murewa <adebajomurewa@gmail.com>
2026-03-29 17:03:33 -0400

    Fixing form draft 2

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,37 +1,13 @@
 import "../css/Help.css";
 import Map from "../components/Map";
+import ContactForm from "../components/ContactForm";
 
 const Help = () => {
     return (
     <body>
         <main>
             <div className="box" id="contact-form-box">
-                <h1>Contact Form</h1>
-                <p>Response time is from 1 to 2 business days</p>
-
-                <form id="contact-form" action="https://api.web3forms.com/submit" method="POST">
-
-                    <input type="hidden" name="access_key" value="a873e42a-e7cc-4fe6-8e7d-c6db872b0e77"/>
-
-                    <div className="form-group">
-                         <label>Name:</label>
-                        <input type="text" name="user-name" id="user-name" required/>
-                    </div>
-                    <div className="form-group">
-                        <label>Email:</label>
-                        <input type="email" name="user-email" id="user-email" required/>
-                    </div>
-                    <div className="form-group">
-                        <label>Message:</label>
-                        <textarea name="message" id="message"></textarea>
-
-                        <div id="submission-result">
-                            
-                        </div>
-                    </div>
-
-                    <p><button type="submit" id="submit-btn">Submit Form</button></p>
-                </form>
+                <ContactForm/>
             </div>
             <div className="box" id="google-map">
                 <Map/>
