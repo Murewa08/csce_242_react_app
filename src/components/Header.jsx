ea930065970d4859e55f4e373dc583ae158d5839
Murewa <adebajomurewa@gmail.com>
2026-03-30 14:10:02 -0400

    Fixing toggle nav draft 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,10 @@ const Header = () => {
     return (
         <header id="main-header">
             <h1>Meradiya - Find Your Paradise</h1>
+            <div id="nav-toggle">
+                <button id="toggle-open" className="toggle">Open &rarr;</button>
+                <button id="toggle-close" className="toggle">Close &larr;</button>
+            </div>
             <Navigation />
         </header>
     );
